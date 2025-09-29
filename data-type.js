36323757e0762eb3b22944d230211f94b4c57f29
tania <tania.190142@s.pust.ac.bd>
2025-09-29 19:11:19 +0600

    basic

---
+++ data-type.js
@@ -0,0 +1,28 @@
+ var firstName = "tanni";
+        console.log(firstName);
+        console.log("Name:Tani");
+        console.log("Income:",50000);
+
+        var num1 = 20;
+        num1 = toString(num1);
+        num1 = parseInt(num1);
+        num1 = parseFloat(num1);
+        console.log(typeof(num1));
+
+
+        var num2 = 2.5678;
+        console.log(num2.toFixed(3));
+        console.log(num2.toPrecision(3));
+        console.log(Number("12"))
+        console.log(Number(true))
+        
+
+
+        const userName = "Taimur Rahman";
+        const income = 3750.25;
+        const expense = 2580.5;
+        console.log('Pewrsonal budget Tracker app');
+        console.log('User: ' + userName.toUpperCase());
+        console.log('Income:' + income);
+        console.log('Expense:' + expense);
+        console.log('Balance:' + (income - expense));
